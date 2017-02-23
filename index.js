0ff97b48e729ca1d81d4ffc219728f5a65fa8f47
Sergiu Lucec <dreagsharr@yahoo.de>
2017-02-23 15:25:28 +0000

    ip fix

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 8080;
 var bodyParser = require('body-parser');
-var ip = require("ip");
 
 app.get('/',(req,res)=>{
+    var ip = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
     var lang = req.headers["accept-language"].split(",")[0];
     var software = req.headers["user-agent"].match(/\((.+?)\)/)[1];
-    var out = {"ipaddress":ip.address(),"language":lang,"software":software};
-   res.send(out);
+    var out = {"ipaddress":ip,"language":lang,"software":software};
+   res.send(out); 
 });
 
 app.get("/",(req,res)=>{
